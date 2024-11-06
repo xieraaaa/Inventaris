e2d65ae0d579b352b9fc6c165a63483897360951
Reifan putra <reifanps64@gmail.com>
2024-11-06 10:57:24 +0700

    tampilan login

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\UnitController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Route::get('/dashboard', function () {
